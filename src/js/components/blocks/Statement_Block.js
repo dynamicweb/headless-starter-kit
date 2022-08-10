281ed79566d8cc9b4d55f30f208148296d6a48cd
Stefan Winther <mr.winther@gmail.com>
2022-08-10 10:52:18 +0200

    Statement block
    - fine tuning

--- src/js/components/blocks/Statement_Block.js
+++ src/js/components/blocks/Statement_Block.js
@@ -10,10 +10,10 @@ export default class StatementBlock extends BlockBase {
 			<div class="card d-flex shadow">
 				<img class="avatar" src="${this.getImageUrl(this.getFieldValue('Image')[0].Path, 100, 75)}" alt="Avatar" style="aspect-ratio: 1/1; object-fit: cover;">
 				<div class="d-flex flex-column">
-					<h4 class="quote fs-6 fw-bold">${this.getFieldValue('Text')}</h4>
+					<h4 class="quote fs-5 fw-bold">${this.getFieldValue('Text')}</h4>
 					<div>
 						<h6 class="fs-4">${this.getFieldValue('Title')}</h6>
-						<div class="cite text-p-4">${this.getFieldValue('Cite')}</div>		
+						<div class="cite text-p-4 fs-3">${this.getFieldValue('Cite')}</div>		
 					</div>
 				</div>
 			</div>
